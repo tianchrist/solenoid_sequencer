bfef6ae322cc8dad8736024fb0ccbc4519752b8e
tianchrist <tianchrist@gmail.com>
2024-06-12 23:14:41 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
-let seq = []
+// 120 bpm means 120 quarter beats
+let masterTempo = 120
+let lastTime = 0
+// sub division of step lengths (0->quarter beat, 1->eighth, 2->sixteenth)
+let subDiv = 0
+// time step for every beat
+let stepLengthms = (60000 / masterTempo) >> subDiv
+
+
+let seq = [false, false, false, false]
 basic.forever(function () {
+    
     pins.digitalWritePin(DigitalPin.P0, 1)
     basic.showLeds(`
     . . . . .
